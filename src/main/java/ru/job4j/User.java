26050bb622cce09a1402db69b60d4d3c9d8a5a27
Антон Юн <crazy_antonio@mail.ru>
2025-03-12 17:19:59 +0300

    5. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/User.java
+++ src/main/java/ru/job4j/User.java
@@ -3,6 +3,7 @@ package ru.job4j;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class User {
 
@@ -14,6 +15,20 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        User user = (User) o;
+//        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+//    }
+
+    @SuppressWarnings("checkstyle:EqualsHashCode")
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 
     public static void main(String[] args) {
