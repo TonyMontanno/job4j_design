526061235981a5a958746fa8532ce87c4f083879
Антон Юн <crazy_antonio@mail.ru>
2025-03-19 17:03:26 +0300

    1. Создать элементарную структуру дерева [#1711]

---
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -0,0 +1,67 @@
+package ru.job4j.tree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SimpleTreeTest {
+
+    @Test
+    void when6ElFindLastThen6() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.findBy(6)).isPresent();
+    }
+
+    @Test
+    void whenElFindNotExistThenOptionEmpty() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        assertThat(tree.findBy(7)).isEmpty();
+    }
+
+    @Test
+    void whenChildExistOnLeafThenNotAdd() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.add(2, 6)).isFalse();
+    }
+
+    @Test
+    void whenParentNotExistThenReturnFalse() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        assertThat(tree.add(3, 4)).isFalse();
+    }
+
+    @Test
+    void whenChildNotExistThenReturnTrue() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        assertThat(tree.add(1, 4)).isTrue();
+    }
+
+    @Test
+    void whenChildWasAddedAndFindByThenReturnTrue() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        assertThat(tree.findBy(2).isPresent()).isTrue();
+    }
+
+    @Test
+    void whenChildDidNotAddedAndFindByThenReturnFalse() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        boolean addedAgain = tree.add(1, 2);
+        assertThat(tree.findBy(2)).isPresent();
+        assertThat(addedAgain).isFalse();
+    }
+}
