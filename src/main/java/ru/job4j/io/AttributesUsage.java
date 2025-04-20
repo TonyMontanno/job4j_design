10e54d99102111cb9db9f828721570035ecabda8
Антон Юн <crazy_antonio@mail.ru>
2025-04-20 22:00:10 +0300

    10. Сканирование файловой системы [#106929]

--- src/main/java/ru/job4j/io/AttributesUsage.java
+++ src/main/java/ru/job4j/io/AttributesUsage.java
@@ -8,8 +8,8 @@ import java.nio.file.attribute.BasicFileAttributes;
 
 public class AttributesUsage {
     public static void main(String[] args) throws IOException {
-//        Path file = Path.of("Attributes.txt");
-//        Files.createFile(file);
+        Path file = Path.of("Attributes.txt");
+        Files.createFile(file);
         BasicFileAttributes attributes = Files.readAttributes(Path.of("Attributes.txt"), BasicFileAttributes.class);
 
         System.out.println("Это обычный файл? " + attributes.isRegularFile());
