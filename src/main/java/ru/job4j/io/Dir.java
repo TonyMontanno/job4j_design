0e28783e521ec18c0dd33902845750ddf8bef353
Антон Юн <crazy_antonio@mail.ru>
2025-04-23 21:30:42 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -1,31 +1,24 @@
 package ru.job4j.io;
 
 import java.io.File;
+import java.util.Objects;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("c:\\projects");
-        if (!file.exists()) {
-            throw new IllegalArgumentException(String.format("Директория не существует: %s", file.getAbsoluteFile()));
-        }
-        if (!file.isDirectory()) {
-            throw new IllegalArgumentException(String.format("Это не директория: %s", file.getAbsoluteFile()));
-        }
-        System.out.println(String.format("Размер директории: %s", file.getTotalSpace()));
-        System.out.println();
-        listFiles(file);
-    }
+        File file = new File(args[0]);
 
-    private static void listFiles(File directory) {
-        File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isFile()) {
-                    System.out.println(String.format("Имя файла: %s, Размер файла: %s", file.getName(), file.length()));
-                } else if (file.isDirectory()) {
-                    listFiles(file);
-                }
-            }
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+
+        System.out.printf("size : %s%n", file.getTotalSpace());
+
+        for (File subfile : Objects.requireNonNull(file.listFiles())) {
+            System.out.println(subfile.getName());
         }
     }
 }
--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,34 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validateArgs(args);
+
+        Path start = Paths.get(args[0]);
+        String extension = args[1];
+
+        search(start, path -> path.toFile().getName().endsWith(extension)).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void validateArgs(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalStateException("Аргументов должно быть два");
+        }
+
+        Path startPath = Paths.get(args[0]);
+        if (!Files.exists(startPath) || !Files.isDirectory(startPath)) {
+            throw new IllegalStateException("Первый аргумент должен быть существующей директорией");
+        }
+
+        String extension = args[1];
+        if (!extension.startsWith(".") || !extension.matches("\\.[a-zA-Z0-9]+")) {
+            throw new IllegalStateException("Второй аргумент должен быть расширением файла, начинающимся с точки.");
+
+        }
     }
 }
