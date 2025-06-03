0a500726709d4f28c5853fbe5c4fb40d609af65f
Антон Юн <crazy_antonio@mail.ru>
2025-06-03 16:31:42 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,25 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        byte favoriteNumber = 9;
+        short grade = 11;
+        int height = 186;
+        float weight = 85.4F;
+        double salary = 527000.0;
+        long carMileage = 268000L;
+        char gender = 'M';
+        boolean isMarried = true;
+
+        LOG.debug("User info name : {}, age : {}", name, age);
+        LOG.debug("User name: {}, favorite number: {}", name, favoriteNumber);
+        LOG.debug("User name: {}, grade: {}", name, grade);
+        LOG.debug("User name: {}, height: {}", name, height);
+        LOG.debug("User name: {}, weight: {}", name, weight);
+        LOG.debug("User name: {}, salary: {}", name, salary);
+        LOG.debug("User name: {}, carMileage: {}", name, carMileage);
+        LOG.debug("User name: {}, gender: {}", name, gender);
+        LOG.debug("User name: {}, isMarried: {}", name, isMarried);
     }
 }
